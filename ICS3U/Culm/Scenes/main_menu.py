b252f36cdd985817a737a97aa28c2dff0b17076f
Robert Abraham <robertsamiabraham@gmail.com>
2024-06-11 00:19:57 -0400

    Di

--- ICS3U/Culm/Scenes/main_menu.py
+++ ICS3U/Culm/Scenes/main_menu.py
@@ -13,6 +13,10 @@ from panda3d.core import TransparencyAttrib
 
 class MainMenu(Stage):
     def enter(self, d):
+        # Owned Countries in this order: AFR, ANT, AUS, EUR, IND, NOA, SOA
+        self.owned_countries = [1,1,1,0,0,0,0]
+        self.country_codes = ["AFR", "ANT", "AUS", "EUR", "IND", "NOA", "SOA"]
+        self.countries_map = [0,0,0,0,0,0,0]
         self.imgMAP = \
             OnscreenImage(
                 image="assets/Map/MAP.png",
@@ -24,17 +28,23 @@ class MainMenu(Stage):
         base.accept("escape", sys.exit)  # Escape quits
 
         self.coloured_countries()
-    def coloured_countries(self, COUNTRY_CODE=0):
-        self.Contry = \
-            OnscreenImage(
-                image="assets/Map/EUR.png",
-                parent=base.pixel2d,
-                scale=(897/base.scalebg, 0, 1003/base.scalebg),  # Image size
-                pos=(base.resx/2, 1, -base.resy/2),  # Window size
-                
-            )
-        self.Contry.setTransparency(TransparencyAttrib.MAlpha)
-        
+
+    def coloured_countries(self):
+        for country_num ,country in enumerate(self.owned_countries):
+            if country == True:
+                print("dickhead", self.country_codes[country_num], country)
+                self.countries_map[country_num]= \
+                    OnscreenImage(
+                        image=f"assets/Map/{self.country_codes[country_num]}.png",
+                        parent=base.pixel2d,
+                        scale=(897/base.scalebg, 0, 1003/base.scalebg),  # Image size
+                        pos=(base.resx/2, 1, -base.resy/2),  # Window size
+                        
+                    )
+                self.countries_map[country_num].setTransparency(TransparencyAttrib.MAlpha)
+
+            
+       
     def exit(self,data):
         for entity in self.entities:
             entity.destroy()
